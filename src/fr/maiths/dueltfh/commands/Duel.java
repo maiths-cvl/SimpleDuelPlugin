8fa5493dd6ee36bdd391083707ff823f0c19045b
Capitaine-Vole <capitainevole@gmail.com>
2022-10-18 20:16:23 +0200

    update

--- src/fr/maiths/dueltfh/commands/Duel.java
+++ src/fr/maiths/dueltfh/commands/Duel.java
@@ -18,21 +18,29 @@ public class Duel implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			if (args.length > 0) {
-				if (Bukkit.getServer().getPlayer(args[0]) != null && Bukkit.getServer().getPlayer(args[0]).getName().equalsIgnoreCase(args[0])) {
-					player.sendMessage(ChatColor.YELLOW + args[0] + ChatColor.WHITE + " is online !!");
-					
-					TextComponent message = new TextComponent("Click me");
-					message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org"));
-					message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Visit the Spigot website!").create()));
-					message.addWith("Maiths");
-					
+			//if (player.hasPermission("duel.send")) {
+				if (args.length > 0) {
+					if (Bukkit.getServer().getPlayer(args[0]) != null && Bukkit.getServer().getPlayer(args[0]).getName().equalsIgnoreCase(args[0])) {
+						player.sendMessage(ChatColor.YELLOW + args[0] + ChatColor.WHITE + " is online !!");
+						
+						TextComponent message = new TextComponent("You can click here");
+						message.setColor(ChatColor.GOLD);
+						message.setBold(true);
+						message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/say hi")); // problem to run tp command as an op without having perms
+						message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, 
+								new ComponentBuilder("Opens you hypixel's jobs page").color(ChatColor.YELLOW).create()));
+						player.spigot().sendMessage(message);
+						
+					} else {
+						player.sendMessage(ChatColor.RED + "This player is offline");
+					}
 				} else {
-					player.sendMessage(ChatColor.RED + "This player is offline");
+					player.sendMessage(ChatColor.RED + "Error witht the syntax, try : /duel {playername}");
 				}
-			} else {
-				player.sendMessage(ChatColor.RED + "Error witht the syntax, try : /duel {playername}");
-			}
+			/*} else {
+				player.sendMessage(ChatColor.RED + "You don't have the permission to run this command");
+			}*/
+			
 		} else {
 			sender.sendMessage(ChatColor.RED + "You are not a player");
 		}
@@ -40,3 +48,12 @@ public class Duel implements CommandExecutor {
 	}
 
 }
+
+/*
+TextComponent message = new TextComponent("You can click here");
+message.setColor(ChatColor.GOLD);
+message.setBold(true);
+message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.hypixel.net/jobs"));
+message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, 
+		new ComponentBuilder("Opens you hypixel's jobs page").color(ChatColor.YELLOW).create()));
+player.spigot().sendMessage(message);*/
